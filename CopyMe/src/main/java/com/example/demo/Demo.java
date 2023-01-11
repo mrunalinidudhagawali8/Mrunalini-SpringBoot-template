a61c544d9fb472663d59d9e8d16a6c806bf69f0f
Mrunalini <dudhagawali@gmail.com>
2023-01-11 22:26:22 +0530

    CopyMe

--- CopyMe/src/main/java/com/example/demo/Demo.java
+++ CopyMe/src/main/java/com/example/demo/Demo.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Hey there good noob!");
+		System.out.println("Hey there good noob! good job on first commit :)");
 	}
 }
